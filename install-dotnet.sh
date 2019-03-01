75422cfd2d1d4cbff5f20330b7f9fbddf71d6cf7
Mike Harder <mharder@microsoft.com>
2019-03-01 14:39:33 -0800

    Update to .NET Core 2.2 (#2)

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -3,11 +3,12 @@
 # echo on
 set -x
 
-# https://www.microsoft.com/net/core#linuxubuntu
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+# https://dotnet.microsoft.com/download/linux-package-manager/ubuntu18-04/sdk-current
+wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
-
+sudo add-apt-repository universe
+sudo apt-get -y install apt-transport-https
 sudo apt-get update
-sudo apt-get -y install dotnet-sdk-2.0.0
+sudo apt-get -y install dotnet-sdk-2.2
